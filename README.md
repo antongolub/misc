9389df0c5259f4e0ec1256fbbaa240e8cd7c6d5c
Anton Golub <antongolub@antongolub.com>
2023-05-29 22:33:04 +0300

    build: add readme toc update script

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 ## Contents
 | Package | Description | Latest |
 |---------|-------------|--------|
+| @antongolub/blank | Blank TS package | [![npm (scoped)](https://img.shields.io/npm/v/@antongolub/blank)](https://www.npmjs.com/package/@antongolub/blank) |
+| @antongolub/infra | Repo infra assets |  |
 
 ## License
 [MIT](./LICENSE)
---
+++ packages/infra/scripts/readme-toc.mjs
@@ -0,0 +1,25 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import {topo} from '@semrel-extra/topo'
+import {gitRoot} from '@antongolub/git-root'
+
+const root = await gitRoot()
+const {packages} = await topo({cwd: root})
+const readmeFile = path.resolve(root, 'README.md')
+let readmeContents = await fs.readFile(readmeFile, 'utf8')
+
+const digest = Object.values(packages)
+  .sort(({name: a}, {name: b}) => a.localeCompare(b))
+  .reduce((m, {manifest: {name, description, private: _private}}) => {
+    const badge = _private
+      ? ''
+      : `[![npm (scoped)](https://img.shields.io/npm/v/${name})](https://www.npmjs.com/package/${name})`
+
+    return m + `\n| ${name} | ${description} | ${badge} |`
+  }, '| Package | Description | Latest |\n|---------|-------------|--------|')
+
+readmeContents = readmeContents.replace(/(## Contents)([\s\S]+)(##)/, (_, pre, __, post) =>
+  `${pre}\n${digest}\n\n${post}`
+)
+
+await fs.writeFile(readmeFile, readmeContents, 'utf8')
