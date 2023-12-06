f59e2ee1bd1c735e404b6f5eab4e9600cd361159
Anton Golub <antongolub@antongolub.com>
2023-12-06 14:47:50 +0300

    feat(upkeeper): implement pkg json updater

--- packages/upkeeper/src/main/ts/index.ts
+++ packages/upkeeper/src/main/ts/index.ts
@@ -1,8 +1,10 @@
 import cp from 'node:child_process'
 import fs from 'node:fs/promises'
 import path from 'node:path'
+import semver from 'semver'
 
 const defaultScopes = ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies']
+
 export const upkeeper = async ({
   cwd = process.cwd(),
   target = 'package.json',
@@ -14,14 +16,19 @@ export const upkeeper = async ({
   const pkgJson = JSON.parse(await fs.readFile(path.resolve(cwd, target), 'utf8'))
   const deps = getDeps(pkgJson)
   const _deps = filterDeps(deps, ignore, match, scope)
-  // const versions = await getVersions()
+  const versions = await getVersionsMap(_deps)
+  const __deps = updateDeps(_deps, versions)
+  const _pkgJson = updatePkgJson(pkgJson, __deps)
+
+  return _pkgJson
 }
 
+export type TPkgJson = Record<string, any>
 export type TDeps = [string, string, string][]
 export type TVersions = string[]
 export type TVersionsMap = Record<string, string[]>
 
-export const getDeps = (pkgJson: Record<string, any>): TDeps => {
+export const getDeps = (pkgJson: TPkgJson): TDeps => {
   const deps: TDeps = []
   for (const scope of defaultScopes) {
     for(const [name, version] of Object.entries(pkgJson[scope] || {})) {
@@ -47,13 +54,30 @@ export const filterDeps = (deps: TDeps, ignore: string | string[], match: string
 
   return deps.filter(([n, v, s]) => scopes.includes(s) && (matched.length === 0 || matched.includes(n)) && !ignored.includes(v))
 }
+
+export const updateDeps = (deps: TDeps, versions: TVersionsMap): TDeps =>
+  deps.map(([name, version, scope]) => {
+    const _version = getLatestCompatibleVersion(version, versions[name]) || version
+    return [name, _version, scope]
+  })
+
+export const getLatestCompatibleVersion = (v: string, versions: string[]): string | undefined => {
+  const caret = v.startsWith('^') || v.startsWith('~') ? v[0] : ''
+  return caret + versions.find(_v => semver.satisfies(_v, v))
+}
+
+export const updatePkgJson = (pkg: TPkgJson, deps: TDeps) => {
+  const _pkg: TPkgJson = JSON.parse(JSON.stringify(pkg))
+  for(const [name, version, scope] of deps) {
+    _pkg[scope][name] = version
+  }
+
+  return _pkg
+}
+
 export const asArray = (value: string | string[]) => [value].flat().flatMap(i => i.split(','))
-// export const getScopes = (pkgJson: Record<string, any>, scope: string): [string, string][] => {
-//   const names = scope === '*' ? ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies'] : [scope].flat()
-//
-// }
-// export const getDeps = (scopes: [string, string[]][], pick: string[], omit: string[]) => ([])
-export const getVersion = async (name: string): Promise<TVersions> => JSON.parse((await spawn('npm', ['view', name, 'versions', '--json'], {silent: true})).stdout)
+
+export const getVersion = async (name: string): Promise<TVersions> => JSON.parse((await spawn('npm', ['view', name, 'versions', '--json'], {silent: true})).stdout).sort(semver.rcompare)
 
 export const spawn = (
   cmd: string,
--- packages/upkeeper/src/test/ts/index.test.ts
+++ packages/upkeeper/src/test/ts/index.test.ts
@@ -1,6 +1,15 @@
 import * as assert from 'node:assert'
 import { describe, it } from 'node:test'
-import {filterDeps, getDeps, getVersion, getVersionsMap, TDeps, upkeeper} from '../../main/ts'
+import {
+  filterDeps,
+  getDeps,
+  getLatestCompatibleVersion,
+  getVersion,
+  getVersionsMap,
+  TDeps,
+  updateDeps, updatePkgJson,
+  upkeeper
+} from '../../main/ts'
 
 describe('upkeeper()', () => {
   it('is callable', () => {
@@ -57,5 +66,61 @@ describe('`getVersionsMap`', () => {
     ]
     const versions = await getVersionsMap(deps)
     assert.ok(versions['lcov-utils'].includes('0.0.1'))
+  })
+})
+
+
+describe('`updateDeps`', () => {
+  it('update deps by the specified rules', async () => {
+    const deps: TDeps = [
+      ['lcov-utils', '^1.0.0', 'dependencies'],
+      ['topoconfig', '^2.0.0', 'devDependencies']
+    ]
+    const versions = {
+      'lcov-utils': ['1.2.3', '1.0.0', '0.0.1', '0.0.0'],
+      'topoconfig': ['3.0.0', '2.5.0', '2.0.0', '1.0.0']
+    }
+
+    const _deps = updateDeps(deps, versions)
+
+    assert.ok(_deps[0][1] === '^1.2.3')
+    assert.ok(_deps[1][1] === '^2.5.0')
+  })
+})
+
+describe('`getLatestCompatibleVersion`', () => {
+  it('returns latest range-compatible version of the specified set', () => {
+    const range = '^1.0.0'
+    const versions = ['2.0.0', '1.2.3', '1.1.0', '1.0.0']
+    const found = getLatestCompatibleVersion(range, versions)
+
+    assert.equal(found, '^1.2.3')
+  })
+})
+
+describe('`updatePkgJson`', () => {
+  it('returns updated pkg json', () => {
+    const pkg = {
+      dependencies: {
+        foo: '^1.0.0'
+      },
+      devDependencies: {
+        bar: '^2.0.0'
+      }
+    }
+    const deps: TDeps = [
+      ['foo', '^1.2.3', 'dependencies'],
+      ['bar', '^2.5.0', 'devDependencies']
+    ]
+    const _pkg = updatePkgJson(pkg, deps)
+
+    assert.deepEqual(_pkg, {
+      dependencies: {
+        foo: '^1.2.3'
+      },
+      devDependencies: {
+        bar: '^2.5.0'
+      }
+    })
   })
 })
