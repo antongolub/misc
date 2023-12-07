781c4f5c4d9cc031d1a30298ef8559d8c616cf7c
Anton Golub <antongolub@antongolub.com>
2023-12-07 10:48:13 +0300

    feat(upkeeper): provide `output` option

--- packages/upkeeper/src/main/ts/cli.ts
+++ packages/upkeeper/src/main/ts/cli.ts
@@ -3,18 +3,25 @@
 import {upkeeper} from './index.js'
 import minimist from 'minimist'
 import process from 'node:process'
+import fs from 'node:fs/promises'
+import path from 'node:path'
 
 const flags = minimist(process.argv.slice(2), {
   boolean: ['dryRun'],
-  string: ['cwd', 'target', 'scope', 'ignore', 'match', 'commit']
+  string: ['cwd', 'target', 'scope', 'ignore', 'match', 'commit', 'output']
 });
 
 (async () => {
   try {
     const chunks = await upkeeper(flags)
+    const cwd = flags.cwd || process.cwd()
     const script = chunks.join('\n')
 
-    console.log(script)
+    if (flags.output) {
+      await fs.writeFile(path.resolve(cwd, flags.output), script, 'utf8')
+    } else {
+      console.log(script)
+    }
     process.exit(0)
   } catch (e) {
     console.error(e)
