e329779eb4b463c3a61085700789ca198a8a35f7
Anton Golub <antongolub@antongolub.com>
2023-12-11 13:49:58 +0300

    perf(upkeeper): memoize `getVersions`

--- packages/upkeeper/src/main/ts/util.ts
+++ packages/upkeeper/src/main/ts/util.ts
@@ -68,5 +68,17 @@ export const tpl =(input: string, ctx: Record<string, any> = {}) => {
 export const asArray = (input: string | string[]): string[] => [input]
   .flat()
   .filter(Boolean)
-  .map(s => s.split(','))
-  .flat()
+  .flatMap(s => s.split(','))
+
+export const memoize = <T extends (...args: any[]) => any>(fn: T): T => {
+  const cache = new Map()
+  return ((...args: any[]) => {
+    const key = JSON.stringify(args)
+    if (cache.has(key)) {
+      return cache.get(key)
+    }
+    const result = fn(...args)
+    cache.set(key, result)
+    return result
+  }) as any
+}
