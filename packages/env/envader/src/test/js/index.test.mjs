4b4bc82e21aa856798387dd06558fffcb02c2a8d
Anton Golub <antongolub@antongolub.com>
2023-12-14 15:55:53 +0300

    chore(envader): rm useless tests

--- packages/env/envader/src/test/js/index.test.mjs
+++
@@ -1,9 +0,0 @@
-import assert from 'node:assert'
-import { describe, it } from 'node:test'
-import { foo } from '@antongolub/blank'
-
-describe('mjs foo()', () => {
-  it('is callable', () => {
-    assert.equal(foo(), undefined)
-  })
-})
