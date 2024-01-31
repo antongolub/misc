42f8759aad397001745b1e5e812bfee2d4d281af
Anton Golub <antongolub@antongolub.com>
2024-01-31 22:05:47 +0300

    fix(dts-fix): handle findBase corner cases

--- packages/dep/tsc-dts-fix/src/main/ts/util.ts
+++ packages/dep/tsc-dts-fix/src/main/ts/util.ts
@@ -1,3 +1,4 @@
+import path from 'node:path'
 
 export const camelizeRecord = (flags: Record<string, any>) =>
   Object.fromEntries(Object.entries(flags).map(([k, v]) => ([
@@ -7,12 +8,13 @@ export const camelizeRecord = (flags: Record<string, any>) =>
 
 export const findBase = (files: string[]) => {
   const first = files[0]
+  const dirname = (f: string) => f.endsWith('/') ? f : path.dirname(f) + '/'
 
   if (files.length === 0) return ''
-  if (files.length === 1) return first.slice(0, first.lastIndexOf('/') + 1)
+  if (files.length === 1) return dirname(first)
 
   // eslint-disable-next-line
-  return first.slice(0, first.split('').findIndex((c, i) => files.some(f => f.charAt(i) !== c)))
+  return dirname(first.slice(0, first.split('').findIndex((c, i) => files.some(f => f.charAt(i) !== c))))
 }
 
 export const logValue = (any: any) => { console.log(any); return any }
