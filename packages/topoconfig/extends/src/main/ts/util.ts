d3f24ac25db5204594bdfeb98cdb3d67ee8628c8
Anton Golub <antongolub@antongolub.com>
2024-02-15 23:24:48 +0300

    refactor(xtends): introduce `ctx` as optional clone arg

--- packages/topoconfig/extends/src/main/ts/util.ts
+++ packages/topoconfig/extends/src/main/ts/util.ts
@@ -1,4 +1,5 @@
 import util from 'node:util'
+import {TCloneCtx, TCloneOpts} from './interface.js'
 
 export const isString = (value: any): value is string => typeof value === 'string'
 
@@ -24,36 +25,36 @@ export const getSeed = (value: any) => isCloneable(value)
 
 export const getProps = (value: any) => [...Object.getOwnPropertyNames(value), ...Object.getOwnPropertySymbols(value)]
 
-export const clone = <T = any>(value: T, vmap?: CloneCtx<T>['vmap']): T =>
-  _clone<T>({value, vmap})
-
-type CloneCtx<T> = {
-  value: T
-  memo?: Map<any, any>
-  seed?: any
-  vmap?: (value: any, key: string | symbol, prefix: string, root: any) => any
-  prefix?: string
-  root?: any
-}
+export const clone = <T = any>(value: T, {vmap, cwd, id}: TCloneOpts<T> = {}): T =>
+  _clone<T>({value, cwd, vmap, id})
 
 export const _clone = <T = any>({
   value,
   memo = new Map(),
   seed = getSeed(value),
-  vmap = v => v,
+  vmap = ({value}) => value,
   prefix = '',
-  root = value
-}: CloneCtx<T>): T => seed
+  root = value,
+  cwd = process.cwd(),
+  id
+}: TCloneCtx<T>): T => seed
   ? getProps(value).reduce((m: any, k) => {
     const p = `${prefix}${k.toString()}`
-    const v = vmap((value as any)[k], k, p, root)
+    const v = vmap({
+      value: (value as any)[k],
+      key: k,
+      prefix: p,
+      root,
+      cwd,
+      id
+    })
     if (memo.has(v)) {
       m[k] = memo.get(v)
     } else {
       const _seed = getSeed(v)
       if (_seed) {
         memo.set(v, _seed)
-        _clone({value: v, memo, seed: _seed, vmap, prefix: `${p}.`, root})
+        _clone({value: v, memo, seed: _seed, vmap, prefix: `${p}.`, root, cwd, id})
         m[k] = _seed
       } else {
         m[k] = v
