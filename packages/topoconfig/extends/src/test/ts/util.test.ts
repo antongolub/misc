a83fa33f4afdf8230d4eafca4799f64aec21352a
Anton Golub <antongolub@antongolub.com>
2024-02-17 17:49:39 +0300

    test(xtends): add more tests for utils

--- packages/topoconfig/extends/src/test/ts/util.test.ts
+++ packages/topoconfig/extends/src/test/ts/util.test.ts
@@ -1,6 +1,10 @@
 import * as assert from 'node:assert'
 import { describe, it } from 'node:test'
-import { match } from '../../main/ts/util.ts'
+import {
+  match,
+  getProps,
+  getSeed
+} from '../../main/ts/util.ts'
 
 describe('match()', () => {
   describe('checks strings against patterns', () => {
@@ -25,4 +29,40 @@ describe('match()', () => {
       })
     }
   })
+})
+
+describe('getProps()', () => {
+  const cases: [any, (string|symbol)[]][] = [
+    [{}, []],
+    [{ foo: 1 }, ['foo']],
+    [{ foo: 1, [Symbol.for('test')]: 'test'}, ['foo', Symbol.for('test')]],
+    [[1,2,3], ['0', '1', '2']],
+  ]
+
+  for (const [input, expected] of cases) {
+    it(`should return [${expected.map(v => v.toString()).join(',')}] for ${JSON.stringify(input, null)}`, () => {
+      assert.deepEqual(getProps(input), expected)
+    })
+  }
+})
+
+describe('getSeed()', () => {
+  class Foo {}
+  const cases: [any, any][] = [
+    [{a: 'a'}, {}],
+    [[1,2,3], []],
+    [new Foo(), new Foo()],
+    [new Set(), undefined],
+    [new Map(), undefined],
+    [new Date(), undefined],
+    [/re/, undefined],
+    [Promise.resolve(), undefined],
+    [new Proxy({}, {}), undefined],
+  ]
+
+  for (const [input, expected] of cases) {
+    it(``, () => {
+      assert.deepEqual(getSeed(input), expected)
+    })
+  }
 })
