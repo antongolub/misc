488db29ecd3e7370d9d5c5004934686dab1914c0
Anton Golub <antongolub@antongolub.com>
2024-03-05 21:57:51 +0300

    chore(zurk): add $ to pkg export

--- packages/zurk/src/main/ts/index.ts
+++ packages/zurk/src/main/ts/index.ts
@@ -1,2 +1,4 @@
-export * from './zurk.js'
+export * from './interface.js'
+export { Zurk, zurk } from './zurk.js'
 export { invoke } from './spawn.js'
+export { $ } from './x.js'
--- packages/zurk/src/test/ts/index.test.ts
+++ packages/zurk/src/test/ts/index.test.ts
@@ -1,9 +1,10 @@
 import * as assert from 'node:assert'
 import { describe, it } from 'node:test'
-import { invoke, zurk } from '../../main/ts'
+import { invoke, zurk, $} from '../../main/ts'
 
 describe('index', () => {
   it('has proper exports', () => {
+    assert.equal(typeof $, 'function')
     assert.equal(typeof zurk, 'function')
     assert.equal(typeof invoke, 'function')
   })
